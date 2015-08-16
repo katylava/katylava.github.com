6c715bb8c9fe9bdd8f0b72589dd5e67799007c98
katy lavallee <katylava@gmail.com>
2015-08-15 22:19:34 -0500

    Remove unused home stuff; more instagrams

--- js/home.js
+++ js/home.js
@@ -2,7 +2,7 @@ var feed = new Instafeed({
     accessToken: '53617.060752b.38fb38db6dc141019ad086566185bb8a',
     clientId: '50b63550968847ef8a9fae94aa15e266',
     get: 'user',
-    limit: 6,
+    limit: 9,
     links: true,
     userId: 53617
 });
